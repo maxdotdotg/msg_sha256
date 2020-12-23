280a6b9efcc11aea0de1ff3d29c518c14e5b2c3c
Max G <maxdotdotg@gmail.com>
2020-12-23 09:39:02 -0500

    cleanup import structure and remove debugging print statements

--- server.py
+++ server.py
@@ -1,27 +1,21 @@
 from http.server import BaseHTTPRequestHandler
-from messages import fetch_message, write_message, delete_message
-from metrics import get_metrics
-import logging
 from ast import literal_eval
 
+from messages import fetch_message, write_message, delete_message
+from metrics import get_metrics
 
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         route = self.path.split("/")[1].strip("/")
         if route == "messages":
-            logging.info("hit messages")
             query = fetch_message(self.path.split("/")[2])
             message = query["response"]
             status = query["status"]
         elif route == "metrics":
-            logging.info("hit metrics")
             message = get_metrics()
             status = 200
-        elif route == "":
-            message = "hit the root"
-            status = 200
         else:
-            message = f"not found, yo. path was {self.path} and route was {route}"
+            message = "Not Found"
             status = 404
 
         request_blob = {}
@@ -32,22 +26,19 @@ class Server(BaseHTTPRequestHandler):
 
     def do_POST(self):
         route = self.path.split("/")[1].strip("/")
-        print("route is:", route)
         if route == "messages":
             try:
                 length = int(self.headers["Content-Length"])
                 content = literal_eval(self.rfile.read(length).decode("UTF-8"))
 
-                print("calling write_message with", content["message"])
                 query = write_message(content["message"])
                 message = query["response"]
                 status = query["status"]
-            except TypeError as e:
-                print("caught type error:", e)
+            except TypeError:
                 message = "body must be in the form of { 'message': 'hello there' }"
                 status = 405
         else:
-            message = f"not found, yo. path was {self.path} and route was {route}"
+            message = "Not Found"
             status = 404
 
         self.respond(status, message)
@@ -59,7 +50,7 @@ class Server(BaseHTTPRequestHandler):
             message = query["response"]
             status = query["status"]
         else:
-            message = f"not found, yo. path was {self.path} and route was {route}"
+            message = "Not Found"
             status = 404
 
         self.respond(status, message)
